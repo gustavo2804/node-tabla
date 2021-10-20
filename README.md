f960e7f783cb8dedd6a5f9e0e31569f20a9839cb
Gustavo De oleo <gustavo.deoleor@gmail.com>
2021-10-20 18:32:38 -0400

    README agregado

---
+++ README.md
@@ -0,0 +1,12 @@
+# Notas:
+Este es mi primer programa en node
+```
+Opciones:
+      --help     Muestra ayuda                                        [booleano]
+      --version  Muestra número de versión                            [booleano]
+  -b, --base     este es la base de la tabla                [número] [requerido]
+  -l, --listar   defime si muestra o no en pantalla la tabla
+                                                     [booleano] [defecto: false]
+  -h, --hasta    define hasta donde se desplegara la tabla[número] [defecto: 10]
+
+  ```
--- config/yargs.js
+++ config/yargs.js
@@ -2,17 +2,20 @@ const argv = require('yargs')
                 .option('b',{
                     alias: 'base',
                     type: 'number',
-                    demandOption: true
+                    demandOption: true,
+                    description: 'este es la base de la tabla '
                 })
                 .option('l',{
                     alias: 'listar',
                     type:'boolean',
-                    default: false
+                    default: false,
+                    description: 'defime si muestra o no en pantalla la tabla'
                 })
                 .option('h',{
                     alias: 'hasta',
                     type:'number',
-                    default: 10
+                    default: 10,
+                    description:'define hasta donde se desplegara la tabla'
                 })
                 .check((argv)=>{
                     if(isNaN(argv.b)){
